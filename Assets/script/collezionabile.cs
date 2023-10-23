21f989ab00058f1c8c7f18808fc15491c7e6c05f
listerin18 <alessandro.ale.troiano@gmail.com>
2023-10-23 18:43:05 +0200

    aggiunta collezionabili

---
+++ Assets/script/collezionabile.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class collezionabile : MonoBehaviour
+{
+    public int counter = 0;
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Collezionabile"))
+        {
+            Destroy(collision.gameObject);
+            counter++;
+            Debug.Log("Collezionabili: " + counter);
+        }
+    }
+}
